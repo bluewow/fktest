52e9d81aa90ab41fe528b5a82f5a42fc26f74dc2
bluewow <dogseen@gmail.com>
2023-01-04 18:21:50 +0900

    feat: properties

--- src/main/java/com/example/fkentity/fk/FkController.java
+++ src/main/java/com/example/fkentity/fk/FkController.java
@@ -1,6 +1,7 @@
 package com.example.fkentity.fk;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +16,15 @@ public class FkController {
     private final FkPostRepository fkPostRepository;
     private final FkMemberRepository fkMemberRepository;
 
+    private final EnvConfig config;
+
     @Transactional
     @GetMapping("/test")
     public void test() {
         FkMember fkMember = new FkMember(1L, "member");
         FkPost fkPost = new FkPost(1L, "post", fkMember);
 
+        System.out.println(config.path());
         fkMemberRepository.save(fkMember);
         fkPostRepository.save(fkPost);
     }
